7ac39cf7585cb802f2026f54260efd40554c4e74
Traphume <traphumedev@gmail.com>
2020-05-29 14:58:07 +0700

    Added basic hello world express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
